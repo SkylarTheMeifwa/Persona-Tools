661af631a2de5ec5d5e8f003a894f61e954fad8c
SkylarTheMeifwa <sarce12@lsu.edu>
2025-07-16 05:19:38 -0500

    Update data.js

--- Time-To-Retake-Your-Desires/data.js
+++ Time-To-Retake-Your-Desires/data.js
@@ -1,17 +1,25 @@
-const dailyTasks = [
-  {
-    id: "log-in-raffle",
-    name: "Spin to Win Raffle",
-    categories: ["Login Rewards"]
-  },
-  {
-    id: "daily-loot",
-    name: "Daily Loot", 
-    categories: ["Login Rewards"]
-  },
-  {
-    id: "free-item",
-    name: "Free item in shop"
-    categories: ["Login Rewards"]
-  }
-];
+
+Theives Den: 
+Collect Visitor XP
+Collect Coins in Info
+
+Login Rewards: 
+daily loot
+Free item in shop
+Log in bonus (Spin to win raffle) 
+Coffee aid in Awakening Avenue
+
+Spin to win raffle: 
+Log in bonus (Spin to win raffle) 
+Clear a platform in metro of desire
+Use 120 stamina in realm of repression
+Use 3 city action points
+
+Goals: 
+Use 3 city action points
+Clear realm of repression: enlightenment
+Clear realm of repression: personas
+Complete trials from the sea of souls
+Fuse a persona
+Train a persona's skills
+Use 120 stamina in realm of repression 
