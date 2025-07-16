d1b0eec9bbedb660a7b017cdb72476f9eff2bb1f
SkylarTheMeifwa <sarce12@lsu.edu>
2025-07-16 05:03:30 -0500

    Update data.js

--- Time-To-Retake-Your-Desires/data.js
+++ Time-To-Retake-Your-Desires/data.js
@@ -1,37 +1,17 @@
 const dailyTasks = [
   {
-    id: "velvet-train",
-    name: "Train Persona in Velvet Room",
-    categories: ["Velvet Room", "XP Farming"]
+    id: "log-in-raffle",
+    name: "Spin to Win Raffle",
+    categories: ["Login Rewards"]
   },
   {
-    id: "daily-dungeon",
-    name: "Complete Daily Dungeon",
-    categories: ["Dungeon", "Item Rewards"]
+    id: "daily-loot",
+    name: "Daily Loot", 
+    categories: ["Login Rewards"]
   },
   {
-    id: "leblanc-checkin",
-    name: "Check in at Café Leblanc",
-    categories: ["Social", "XP Farming"]
-  },
-  {
-    id: "boss-practice",
-    name: "Practice Boss Fight",
-    categories: ["Combat", "XP Farming"]
-  },
-  {
-    id: "arena",
-    name: "Participate in the Arena",
-    categories: ["Combat", "Item Rewards"]
-  },
-  {
-    id: "bond-call",
-    name: "Make Bond Call",
-    categories: ["Social"]
-  },
-  {
-    id: "missions",
-    name: "Complete Limited-Time Missions",
-    categories: ["Missions", "Item Rewards"]
+    id: "free-item",
+    name: "Free item in shop"
+    categories: ["Login Rewards"]
   }
-];
+]
