c82dee4c86e4d0c07d1fdd116cd81905fd2c3402
SkylarTheMeifwa <sarce12@lsu.edu>
2025-07-16 16:35:18 -0500

    Update data.js

--- Time-To-Retake-Your-Desires/data.js
+++ Time-To-Retake-Your-Desires/data.js
@@ -1,96 +1,96 @@
-const dailyTasks = [ 
+const dailyTasks = [
   // Login Rewards
   {
     id: "daily-loot",
     name: "Daily loot",
-    categories: ["Login Rewards"]
+    categories: ["Login Rewards"],
   },
   {
     id: "shop-item",
     name: "Free item in shop",
-    categories: ["Login Rewards"]
+    categories: ["Login Rewards"],
   },
   {
     id: "login-bonus",
     name: "Log in bonus (Spin to win raffle)",
-    categories: ["Login Rewards", "Spin to win raffle"]
+    categories: ["Login Rewards", "Spin to win raffle"],
   },
   {
     id: "coffee-aid",
     name: "Coffee aid in Awakening Avenue",
-    categories: ["Login Rewards"]
+    categories: ["Login Rewards"],
   },
 
   // Thieves Den
   {
     id: "visitor-xp",
     name: "Collect Visitor XP",
-    categories: ["Thieves Den"]
+    categories: ["Thieves Den"],
   },
   {
     id: "coins-info",
     name: "Collect Coins in Info",
-    categories: ["Thieves Den"]
+    categories: ["Thieves Den"],
   },
 
   // Goals
   {
     id: "enlightenment-clear",
     name: "Clear Realm of Repression: Enlightenment",
-    categories: ["Goals"]
+    categories: ["Goals"],
   },
   {
     id: "personas-clear",
     name: "Clear Realm of Repression: Personas",
-    categories: ["Goals"]
+    categories: ["Goals"],
   },
   {
     id: "sea-trials",
     name: "Complete trials from the Sea of Souls",
-    categories: ["Goals"]
+    categories: ["Goals"],
   },
   {
     id: "fuse-persona",
     name: "Fuse a persona",
-    categories: ["Goals"]
+    categories: ["Goals"],
   },
   {
     id: "train-persona",
     name: "Train a persona's skills",
-    categories: ["Goals"]
+    categories: ["Goals"],
   },
 
   // Leblanc
   {
     id: "ann",
     name: "Convince Ann to finally talk to me for the third time",
-    categories: ["Leblanc"]
+    categories: ["Leblanc"],
   },
   {
     id: "sojiro",
     name: "Wish that Sojiro was also in Leblanc",
-    categories: ["Leblanc"]
+    categories: ["Leblanc"],
   },
   {
     id: "wonder-ren",
     name: "Have Wonder longingly stare at Ren in Leblanc for at least 30 minutes",
-    categories: ["Leblanc"]
+    categories: ["Leblanc"],
   },
 
   // Spin to Win Raffle
   {
     id: "metro-clear",
     name: "Clear a platform in Metro of Desire",
-    categories: ["Spin to win raffle"]
+    categories: ["Spin to win raffle"],
   },
   {
     id: "stamina-120",
     name: "Use 120 stamina in Realm of Repression",
-    categories: ["Spin to win raffle", "Goals"]
+    categories: ["Spin to win raffle", "Goals"],
   },
   {
     id: "city-action-3",
     name: "Use 3 city action points",
-    categories: ["Spin to win raffle", "Goals"]
-  }
+    categories: ["Spin to win raffle", "Goals"],
+  },
 ];
