8d102a67db7030e2cfb11cb95f474b402e9ec9e5
SkylarTheMeifwa <sarce12@lsu.edu>
2025-07-24 19:52:33 -0500

    Update data.js

--- Time-To-Retake-Your-Desires/data.js
+++ Time-To-Retake-Your-Desires/data.js
@@ -2,22 +2,22 @@ const dailyTasks = [
   // Login Rewards
   {
     id: "daily-loot",
-    name: "Daily loot",
+    name: "Daily loot (Hoot-n-Loot)",
     categories: ["Login Rewards"],
   },
   {
-    id: "shop-item",
-    name: "Free item in shop",
-    categories: ["Login Rewards"],
+    id: "login",
+    name: "Log in to the game (Goals)",
+    categories: ["Goals", "Login Rewards"],
   },
   {
     id: "login-bonus",
-    name: "Log in bonus (Spin to win raffle)",
-    categories: ["Login Rewards", "Spin to win raffle"],
+    name: "Log in bonus (Art of the Fox)",
+    categories: ["Login Rewards"],
   },
   {
-    id: "coffee-aid",
-    name: "Coffee aid in Awakening Avenue",
+    id: "shop-item".
+    name: "Free item in shop",
     categories: ["Login Rewards"],
   },
 
@@ -34,6 +34,21 @@ const dailyTasks = [
   },
 
   // Goals
+  {
+    id: "120stamina",
+    name: "Use 120 Stamina (Realm of Repression)",
+    categories: ["Goals"],
+  },
+  {
+    id: "3action-points",
+    name: "Use 3 City Action Points",
+    categories: ["Goals"],
+  },
+  {
+    id: "alluring-aroma-clear",
+    name: "Clear Realm of Repression: Alluring Aroma",
+    categories: ["Goals"],
+  },
   {
     id: "enlightenment-clear",
     name: "Clear Realm of Repression: Enlightenment",
@@ -55,15 +70,15 @@ const dailyTasks = [
     categories: ["Goals"],
   },
   {
-    id: "train-persona",
-    name: "Train a persona's skills",
+    id: "nightmare-catcher",
+    name: "Reach the 4th level point goal in the Gates of the Nightmare Catcher",
     categories: ["Goals"],
   },
 
   // Leblanc
   {
-    id: "ann",
-    name: "Convince Ann to finally talk to me for the third time",
+    id: "pt-hangout",
+    name: "Hang out with Phantom Thieves for Gems :D",
     categories: ["Leblanc"],
   },
   {
@@ -76,21 +91,132 @@ const dailyTasks = [
     name: "Have Wonder longingly stare at Ren in Leblanc for at least a year without any breaks",
     categories: ["Leblanc"],
   },
+  
+  // Start Rewards
+  {
+    id: "awaken-pt2",
+    name: "Awaken the second Phantom Thief",
+    categories: ["Start Rewards"],
+  },
 
-  // Spin to Win Raffle
+  // Lufel's Plans
   {
-    id: "metro-clear",
-    name: "Clear a platform in Metro of Desire",
-    categories: ["Spin to win raffle"],
+    id: "step5-velvet-trials",
+    name: "Step 5: Clear stage 1 in 2 of the Velvet Trials",
+    categories: ["Lufel's Plans"],
   },
   {
-    id: "stamina-120",
-    name: "Use 120 stamina in Realm of Repression",
-    categories: ["Spin to win raffle", "Goals"],
+    id: "step6-ror-kiuchi",
+    name: "Step 6: Clear Realm of Repression: Kiuchi's Shadow I or higher",
+    categories: ["Lufel's Plans"],
   },
   {
-    id: "city-action-3",
-    name: "Use 3 city action points",
-    categories: ["Spin to win raffle", "Goals"],
+    id: "step6-mementos-aiyatsbus",
+    name: "Step 6: Reach Mementos Path of Aiyatsbus",
+    categories: ["Lufel's Plans"],
+  },
+  {
+    id: "step7-synergy-kayo",
+    name: "Step 7: Unlock Synergy with Kayo Tomiyama",
+    categories: ["Lufel's Plans"],
+  },
+  {
+    id: "step7-synergy-tomoko",
+    name: "Step 7: Unlock Synergy with Tomoko Noge",
+    categories: ["Lufel's Plans"],
+  },
+  {
+    id: "step7-palace2-progress",
+    name: "Step 7: Reach 10% progress in the second palace",
+    categories: ["Lufel's Plans"],
+  },
+  {
+    id: "step7-band-club",
+    name: "Step 7: Clear the mission Rocking with the Band Club 1",
+    categories: ["Lufel's Plans"],
+  },
+  {
+    id: "step7-mission-clear",
+    name: "Step 7: Clear 11 Step 7 Missions",
+    categories: ["Lufel's Plans"],
+  },
+
+  // Revelation Cards
+  {
+    id: "rev-ann",
+    name: "Ann - Strife (4)",
+    categories: ["Revelation Cards"],
+  },
+  {
+    id: "rev-kotone",
+    name: "Kotone - Opulence (4)",
+    categories: ["Revelation Cards"],
+  },
+  {
+    id: "rev-sumi",
+    name: "Sumi - Hindrance (2)",
+    categories: ["Revelation Cards"],
+  },
+  {
+    id: "rev-lufel",
+    name: "Lufel - Love (1)",
+    categories: ["Revelation Cards"],
+  },
+  {
+    id: "rev-yukimi",
+    name: "Yukimi - Peace (1)",
+    categories: ["Revelation Cards"],
+  },
+  {
+    id: "rev-kurotani",
+    name: "Kurotani - Control (1)",
+    categories: ["Revelation Cards"],
+  },
+  {
+    id: "rev-kamiyama",
+    name: "Kamiyama - Power (1)",
+    categories: ["Revelation Cards"],
+  },
+  
+  // Kamoshita's Arc
+  {
+    id: "k-6",
+    name: "Kamoshita Level 6 (55%)",
+    categories: ["Kamoshita's Arc"],
+  },
+  {
+    id: "k-7",
+    name: "Kamoshita Level 7 (62%)",
+    categories: ["Kamoshita's Arc"],
+  },
+  {
+    id: "k-8",
+    name: "Kamoshita Level 8 (94%)",
+    categories: ["Kamoshita's Arc"],
+  },
+  {
+    id: "k-10",
+    name: "Kamoshita Level 10 (90%)",
+    categories: ["Kamoshita's Arc"],
+  },
+  {
+    id: "k-11",
+    name: "Kamoshita Level 11",
+    categories: ["Kamoshita's Arc"],
+  },
+  {
+    id: "k-12",
+    name: "Kamoshita Level 12",
+    categories: ["Kamoshita's Arc"],
+  },
+  {
+    id: "k-13",
+    name: "Kamoshita Level 13",
+    categories: ["Kamoshita's Arc"],
+  },
+  {
+    id: "k-14",
+    name: "Kamoshita Level 14",
+    categories: ["Kamoshita's Arc"],
   },
 ];
