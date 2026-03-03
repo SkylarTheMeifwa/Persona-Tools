5b1bab220d915e34f4acda672f126d8708c86b57
SkylarTheMeifwa <sarce12@lsu.edu>
2026-03-03 15:41:21 -0600

    Add Dropbox OAuth2 authorization handler

---
+++ api/dropbox.js
@@ -0,0 +1,13 @@
+export default function handler(req, res) {
+  const clientId = process.env.DROPBOX_APP_KEY;
+  const redirectUri = "https://persona-tools.vercel.app/dropbox-callback";
+
+  const authUrl =
+    "https://www.dropbox.com/oauth2/authorize" +
+    `?client_id=${clientId}` +
+    "&response_type=code" +
+    "&token_access_type=offline" +
+    `&redirect_uri=${encodeURIComponent(redirectUri)}`;
+
+  res.redirect(authUrl);
+}
